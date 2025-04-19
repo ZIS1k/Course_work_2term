9c93bbc09a9005c57456ba2cfa9fba64342d92e1
ZIS1k <ziro4ka0000@gmail.com>
2025-04-19 18:37:37 +0300

    fix

--- Python/task_1.py
+++
@@ -1,6 +0,0 @@
-import math
-eps = 3.35
-d = 1.5
-D = float(input("Введите D:"))
-Z = 276 / math.sqrt(eps) * math.log( D / d + math.sqrt(1 + D * D / (d * d)))
-print("Волновое сопротивление двухпроводной линии равно =", Z)
